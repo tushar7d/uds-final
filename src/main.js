c723ca2f832ae23b84d371d082d7420ef2cc9891
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2019-12-05 21:47:36 +0530

    changes

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import React from "react";
 import { ToolBar } from "./uds";
 
 export default function Main() {
-  return <ToolBar back />;
+  return <ToolBar />;
 }
--- src/uds/Components/ToolBar.js
+++ src/uds/Components/ToolBar.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import { Stack, Box, Icon, Text } from "../index";
+import { Stack, Icon, Text } from "../index";
 
 const ToolBar = props => {
   return (
@@ -17,12 +17,22 @@ const ToolBar = props => {
     >
       {props.back && (
         <Stack horizontal left mid>
-          <Icon color={"accent.2"}>arrow_back </Icon>
+          <Icon color={"accent.2"}>arrow_back</Icon>
           <Text type400 medium color="neutral__900">
-            {props.title ? props.title : "Title"}
+            {props.title ? props.title : "Toolbar title"}
           </Text>
         </Stack>
       )}
+      {props.close && (
+        <Stack horizontal left mid>
+          <Icon color={"accent.2"}>clear</Icon>
+          <Text type400 medium color="neutral__900">
+            {props.title ? props.title : "Toolbar title"}
+          </Text>
+        </Stack>
+      )}
+      {props.custom && props.children ? props.children : null}
+      {props.icon && <Icon color={"accent.2"}>{props.icon}</Icon>}
     </Stack>
   );
 };
