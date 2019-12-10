7a7eb67073c044bf7dcbee0645f5273ede45b8b3
Tushar Debnath <41472705+tushar7d@users.noreply.github.com>
2019-12-10 14:05:23 +0530

    changes

--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { DetailsPage, Flex, Card, Row } from "./uds";
+import { DetailsPage, Card, Row, Button, Text } from "./uds";
 
 export default function Main() {
   return (
     <DetailsPage>
-      <Card mb="3">
-        <Row center>A Row</Row>
-        <Flex row split bg="neutral__200">
-          <div>hello</div> <div>hello</div>
-        </Flex>
-        <Flex row split bg="neutral__200">
-          <div>hello</div> <div>hello</div>
-        </Flex>
+      <Card mb="3" p="0">
+        <Row center p="4" color="neutral__900">
+          A Row
+        </Row>
+        <Row split p="2">
+          <Button icon="settings" primary>
+            Check
+          </Button>
+          <Text type700 medium color="neutral__900">
+            I am text
+          </Text>
+        </Row>
       </Card>
     </DetailsPage>
   );
